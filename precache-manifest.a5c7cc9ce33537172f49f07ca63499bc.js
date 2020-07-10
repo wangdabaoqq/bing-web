81e06823ca705596e60b567a132ead6958ca3974
wangdabaoqq <wangdabao221@163.com>
2020-07-10 09:24:09 +0000

    Deploying to gh-pages from  @ f35d6a547f91edd2c382a2fe57d9b8a20b8d54ac 🚀

---
+++ precache-manifest.a5c7cc9ce33537172f49f07ca63499bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9e122ad6a66047a3c9df27289c8135",
+    "url": "/wangdabaoqq/bing-web/index.html"
+  },
+  {
+    "revision": "8c548dc252a8e42a7209",
+    "url": "/wangdabaoqq/bing-web/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "75eb590eacc64b51017b",
+    "url": "/wangdabaoqq/bing-web/static/css/main.b200e5d4.chunk.css"
+  },
+  {
+    "revision": "8c548dc252a8e42a7209",
+    "url": "/wangdabaoqq/bing-web/static/js/2.4cfb2733.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wangdabaoqq/bing-web/static/js/2.4cfb2733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eb590eacc64b51017b",
+    "url": "/wangdabaoqq/bing-web/static/js/main.5c6cc2c8.chunk.js"
+  },
+  {
+    "revision": "c9f3465d0c2eb49cc60b",
+    "url": "/wangdabaoqq/bing-web/static/js/runtime-main.228935d4.js"
+  }
+]);
