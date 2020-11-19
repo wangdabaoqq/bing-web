3270356af778f1a207553769ea55d1123ad0f9ec
wangdabaoqq <wangdabao221@163.com>
2020-11-19 14:27:04 +0000

    Deploying to gh-pages from  @ 52e2cea7cefefb4d3a9e9f6822b653742038ebf0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f3f58ef60f028f5ea08abd5f6d37e90.js"
+  "/bing-web/precache-manifest.a7eb982d3f78ed3cb34a269224ce1b90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bing-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
